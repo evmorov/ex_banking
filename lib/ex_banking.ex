e210539d643bcfc709e4579344cdd66fbd2b72c2
Evgeny Morozov <evmorov@gmail.com>
2018-06-07 23:00:30 +0300

    Add typespecs with dialyxir

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,22 +1,39 @@
 defmodule ExBanking do
+  @type banking_error :: {:error,
+    :wrong_arguments                |
+    :user_already_exists            |
+    :user_does_not_exist            |
+    :not_enough_money               |
+    :sender_does_not_exist          |
+    :receiver_does_not_exist        |
+    :too_many_requests_to_user      |
+    :too_many_requests_to_sender    |
+    :too_many_requests_to_receiver
+  }
+
   alias ExBanking.Account
 
+  @spec create_user(user :: String.t) :: :ok | banking_error
   def create_user(user) do
     Account.start_link(user)
   end
 
+  @spec deposit(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | banking_error
   def deposit(user, amount, currency) do
     Account.deposit(user, amount, currency)
   end
 
+  @spec withdraw(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | banking_error
   def withdraw(user, amount, currency) do
     Account.withdraw(user, amount, currency)
   end
 
+  @spec get_balance(user :: String.t, currency :: String.t) :: {:ok, balance :: number} | banking_error
   def get_balance(user, currency) do
     Account.get_balance(user, currency)
   end
 
+  @spec send(from_user :: String.t, to_user :: String.t, amount :: number, currency :: String.t) :: {:ok, from_user_balance :: number, to_user_balance :: number} | banking_error
   def send(from_user, to_user, amount, currency) do
     Account.send(from_user, to_user, amount, currency)
   end
--- mix.exs
+++ mix.exs
@@ -7,22 +7,20 @@ defmodule ExBanking.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_deps: :transitive]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false}
     ]
   end
 end
