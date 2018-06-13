47688c44cf5f3108b9faebb298db87e91dba50b6
Evgeny Morozov <evmorov@gmail.com>
2018-06-13 10:12:40 +0300

    One change_balance/3 instead of deposit/3 + withdraw/3

--- lib/ex_banking/account.ex
+++ lib/ex_banking/account.ex
@@ -26,13 +26,7 @@ defmodule ExBanking.Account do
 
   def deposit(user, amount, currency)
       when is_binary(user) and is_number(amount) and is_binary(currency) do
-    user = String.to_atom(user)
-
-    if Process.whereis(user) do
-      GenServer.call(user, {:deposit, amount, currency})
-    else
-      {:error, :user_does_not_exist}
-    end
+    change_balance(user, amount, currency)
   end
 
   def deposit(_user, _amount, _currency) do
@@ -41,17 +35,21 @@ defmodule ExBanking.Account do
 
   def withdraw(user, amount, currency)
       when is_binary(user) and is_number(amount) and is_binary(currency) do
-    user = String.to_atom(user)
-
-    if Process.whereis(user) do
-      GenServer.call(user, {:withdraw, amount, currency})
-    else
-      {:error, :user_does_not_exist}
-    end
+    change_balance(user, amount * -1, currency)
   end
 
   def withdraw(_user, _amount, _currency) do
     {:error, :wrong_arguments}
+  end
+
+  def change_balance(user, amount, currency) do
+    user = String.to_atom(user)
+
+    if Process.whereis(user) do
+      GenServer.call(user, {:change_balance, amount, currency})
+    else
+      {:error, :user_does_not_exist}
+    end
   end
 
   def get_balance(user, currency) when is_binary(user) and is_binary(currency) do
@@ -67,15 +65,15 @@ defmodule ExBanking.Account do
   def send(from_user, to_user, amount, currency)
       when is_binary(from_user) and is_binary(to_user) and is_number(amount) and
              is_binary(currency) do
-    new_balance_from_user = withdraw(from_user, amount, currency)
+    new_balance_from_user = change_balance(from_user, amount * -1, currency)
 
     if {:error, :user_does_not_exist} == new_balance_from_user do
       {:error, :sender_does_not_exist}
     else
-      new_balance_to_user = deposit(to_user, amount, currency)
+      new_balance_to_user = change_balance(to_user, amount, currency)
 
       if {:error, :user_does_not_exist} == new_balance_to_user do
-        deposit(from_user, amount, currency)
+        change_balance(from_user, amount, currency)
         {:error, :receiver_does_not_exist}
       else
         {new_balance_from_user, new_balance_to_user}
@@ -89,18 +87,10 @@ defmodule ExBanking.Account do
 
   # Server
 
-  def handle_call({:deposit, amount, currency}, _from, account) do
+  def handle_call({:change_balance, amount, currency}, _from, account) do
     amount = (amount / 1) |> Float.round(2)
     current_balance = Map.get(account, currency, 0.0)
     new_balance = current_balance + amount
-    account = Map.put(account, currency, new_balance)
-    {:reply, new_balance, account}
-  end
-
-  def handle_call({:withdraw, amount, currency}, _from, account) do
-    amount = (amount / 1) |> Float.round(2)
-    current_balance = Map.get(account, currency, 0.0)
-    new_balance = current_balance - amount
 
     if new_balance >= 0 do
       account = Map.put(account, currency, new_balance)
